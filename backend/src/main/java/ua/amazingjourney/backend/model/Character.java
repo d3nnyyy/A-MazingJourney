4691f39f32bab65612381361939559bdd5fe3b51
hurrricane <maximgorak2005@gmail.com>
2023-08-11 01:29:03 +0300

    Added visitedCells list to the character characteristics

--- backend/src/main/java/ua/amazingjourney/backend/model/Character.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/Character.java
@@ -1,14 +1,17 @@
 package ua.amazingjourney.backend.model;
 
+import ua.amazingjourney.backend.tools.*;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.LinkedList;
 
 /**
  * The character. Can move
  */
 @Data
-@AllArgsConstructor
 public class Character {
     //The row coordinate of a character
     private int yCoordinate;
@@ -16,11 +19,24 @@ public class Character {
     //The column coordinate of a character
     private int xCoordinate;
 
+    //The list of cells, that were visited by character
+    private LinkedList<Cell> visitedCells;
+
+    public Character(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        visitedCells = new LinkedList<>();
+        visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
+    }
+
     /**
      * Moves character on 1 cell to the right
      */
     public void moveRight() {
         xCoordinate += 2;
+        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
+            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
+        }
     }
 
     /**
@@ -28,6 +44,9 @@ public class Character {
      */
     public void moveLeft() {
         xCoordinate += 2;
+        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
+            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
+        }
     }
 
     /**
@@ -35,6 +54,9 @@ public class Character {
      */
     public void moveUp() {
         yCoordinate += 2;
+        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
+            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
+        }
     }
 
     /**
@@ -42,5 +64,8 @@ public class Character {
      */
     public void moveDown() {
         yCoordinate += 2;
+        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
+            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
+        }
     }
 }
