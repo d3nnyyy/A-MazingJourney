e8f91a994347f23d5f25d6af3e89d2368d061fb7
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-11 13:47:48 +0100

    added back the comment

--- backend/src/main/java/ua/amazingjourney/backend/model/Game.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/Game.java
@@ -6,6 +6,7 @@ public class Game {
     private Maze maze;
     private Character character;
     private GoalPosition goalPosition;
+    //Size of a maze. The value must be odd
     private int size;
 
     private static final int COUNT_OF_CIRCLES = 5;
