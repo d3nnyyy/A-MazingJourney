a9598923c987e715371ab6ae87a5a81fbd8d60e5
hurrricane <maximgorak2005@gmail.com>
2023-08-12 00:30:16 +0300

    Added main method for testing

--- backend/src/main/java/ua/amazingjourney/backend/tools/MazeConverter.java
+++ backend/src/main/java/ua/amazingjourney/backend/tools/MazeConverter.java
@@ -1,6 +1,12 @@
 package ua.amazingjourney.backend.tools;
 
+import java.util.Arrays;
+
 public class MazeConverter {
+
+    public static void main(String[] args) {
+
+    }
 
     public static String convertToMazeString(boolean[][] mazeArray) {
         StringBuilder mazeString = new StringBuilder();
