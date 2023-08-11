5ca8726ee38312a3ce1c4b0d97248064d3f518f0
d3nnyyy <d3nnn41k@gmail.com>
2023-08-11 09:56:50 +0300

    created MazeConverter in order to test generate maze method

---
+++ backend/src/main/java/ua/amazingjourney/backend/tools/MazeConverter.java
@@ -0,0 +1,22 @@
+package ua.amazingjourney.backend.tools;
+
+public class MazeConverter {
+
+    public static String convertToMazeString(boolean[][] mazeArray) {
+        StringBuilder mazeString = new StringBuilder();
+
+        for (int i = 0; i < mazeArray.length; i++) {
+            for (int j = 0; j < mazeArray[i].length; j++) {
+                if (mazeArray[i][j]) {
+                    mazeString.append(" "); // Open path
+                } else {
+                    mazeString.append("#"); // Wall
+                }
+            }
+            mazeString.append("\n");
+        }
+
+        return mazeString.toString();
+    }
+}
+
