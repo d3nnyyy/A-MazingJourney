042416a5872994cdf4841f0ad08501d34fbcd416
hurrricane <maximgorak2005@gmail.com>
2023-08-11 21:08:30 +0300

    Simplified game class

--- backend/src/main/java/ua/amazingjourney/backend/model/Game.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/Game.java
@@ -1,22 +1,20 @@
 package ua.amazingjourney.backend.model;
 
 import lombok.Data;
+import ua.amazingjourney.backend.tools.Cell;
 
 public class Game {
     private Maze maze;
     private Character character;
-    private GoalPosition goalPosition;
+//    private GoalPosition goalPosition;
+    private Cell goalPosition;
     //Size of a maze. The value must be odd
     private int size;
 
     private static final int COUNT_OF_CIRCLES = 5;
     private static final int DEFAULT_CHARACTER_COORDINATE = 0;
 
-    @Data
-    private class GoalPosition {
-        private int x;
-        private int y;
-    }
+
 
     public void startNewGame() {
         character = new Character(DEFAULT_CHARACTER_COORDINATE, DEFAULT_CHARACTER_COORDINATE);
