2e2a24ab856f49216c9bda6872d4768b81f517d8
Nazariy Soloviy <nazariy.soloviy@student.manchester.ac.uk>
2023-08-11 13:36:42 +0100

    added basic input ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import spongebobImage from "./assets/spongebob.png";
+import { Slider, Box, Typography, Tooltip, Button } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
 function App() {
   const maze = [
     [1, 1, 1, 1, 1, 1],
@@ -12,15 +14,42 @@ function App() {
     [1, 1, 1, 1, 1, 1],
   ];
 
+  const sizeMarks = [
+    {
+      value: 10,
+      label: "10",
+    },
+    {
+      value: 15,
+      label: "15",
+    },
+  ];
+
+  const difficultyMarks = [
+    {
+      value: 1, 
+      label: "1"
+    }, 
+    {
+      value: 10,
+      label: '10'
+    }
+  ]
+
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [isMoving, setIsMoving] = useState(false);
-  const [playerPos, setPlayerPos] = useState([0, 0])
+  const [playerPos, setPlayerPos] = useState([0, 0]);
+  const [size, setSize] = useState(12);
+  const [difficulty, setDifficulty] = useState(5);
   const moveDistance = 101.6;
   const moveDelay = 125;
 
   const animationFrame = useRef(null);
   const lastKeyPressTime = useRef(0);
+
+  const handleSizeChange = (event, value) => setSize(value);
+  const handleDifficultyChange = (event, value) => setDifficulty(value);
 
   const handleKeyPress = (event) => {
     const currentTime = Date.now();
@@ -36,22 +65,22 @@ function App() {
       case "KeyW":
       case "ArrowUp":
         setY((prevY) => prevY - moveDistance);
-        setPlayerPos(playerPos => [playerPos[0],(playerPos[1]+1)])
+        setPlayerPos((playerPos) => [playerPos[0], playerPos[1] + 1]);
         break;
       case "KeyS":
       case "ArrowDown":
         setY((prevY) => prevY + moveDistance);
-        setPlayerPos(playerPos => [playerPos[0],(playerPos[1]-1)])
+        setPlayerPos((playerPos) => [playerPos[0], playerPos[1] - 1]);
         break;
       case "KeyA":
       case "ArrowLeft":
         setX((prevX) => prevX - moveDistance);
-        setPlayerPos(playerPos => [(playerPos[0]-1), playerPos[1]])
+        setPlayerPos((playerPos) => [playerPos[0] - 1, playerPos[1]]);
         break;
       case "KeyD":
       case "ArrowRight":
         setX((prevX) => prevX + moveDistance);
-        setPlayerPos(playerPos => [(playerPos[0]+1), playerPos[1]])
+        setPlayerPos((playerPos) => [playerPos[0] + 1, playerPos[1]]);
         break;
       default:
         break;
@@ -59,8 +88,8 @@ function App() {
   };
 
   useEffect(() => {
-    console.log(playerPos)
-  }, [playerPos])
+    console.log(playerPos);
+  }, [playerPos]);
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
@@ -84,39 +113,83 @@ function App() {
 
   return (
     <div className="App">
-      <div className="maze">
-        <div class="rows">
-          {maze.map((row, rowIndex) => (
-            <div key={rowIndex} className="row">
-              {row.map((cell, cellIndex) => (
-                <div
-                  key={cellIndex}
-                  className={`cell ${cell === 0 ? "wall" : "path"} ${
-                    rowIndex === 0 && cellIndex === 0 ? "start" : ""
-                  }`}>
-                
-                  {rowIndex === 0 && cellIndex === 0 && (
+      <div className="app-container">
+        <div className="maze-container">
+          <div class="rows">
+            {maze.map((row, rowIndex) => (
+              <div key={rowIndex} className="row">
+                {row.map((cell, cellIndex) => (
+                  <div
+                    key={cellIndex}
+                    className={`cell ${cell === 0 ? "wall" : "path"} ${
+                      rowIndex === 0 && cellIndex === 0 ? "start" : ""
+                    }`}
+                  >
+                    {rowIndex === 0 && cellIndex === 0 && (
                       <motion.img
-                      initial={{ opacity: 0, scale: 0.5}}
-                      animate={{ opacity: 1, scale: 1, x:x, y:y}}
-                      transition={{ type: "tween"}}
-                      src={spongebobImage}
-                      alt="spongebob"
-                    />
-                    
-                      // <img src={spongebobImage} alt="spongebob" />
-                  )}
-                </div>
-              ))}
-            </div>
-          ))}
+                        className="img-spongebob"
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 1, scale: 1, x: x, y: y }}
+                        transition={{ type: "tween" }}
+                        src={spongebobImage}
+                        alt="spongebob"
+                      />
+                    )}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="input-container">
+          <Box sx={{ width: "25vw" }}>
+            <Box sx={{display:'flex'}}>
+              <Typography>
+                Choose the size of the maze:
+              </Typography>
+              <Tooltip title="The maze is generated as an n*n square, where n is the size of the maze.">
+                  <InfoIcon sx={{width:'15px', height:'15px', color:'#36454F', ml: 0.5, mt: 0.5}} />
+                </Tooltip>
+            </Box>
+
+            <Slider
+              defaultValue={12}
+              min={10}
+              max={15}
+              step={1}
+              marks={sizeMarks}
+              aria-label="Default"
+              valueLabelDisplay="auto"
+              value={size}
+              onChange={handleSizeChange}
+              color="secondary"
+            />
+             <Box sx={{display:'flex'}}>
+              <Typography>
+                Choose the difficulty of the maze:
+              </Typography>
+              <Tooltip title="The higher the difficulty of the maze, the more complex will the generated maze be.">
+                  <InfoIcon sx={{width:'15px', height:'15px', color:'#36454F', ml: 0.5, mt: 0.5}} />
+                </Tooltip>
+            </Box>
+            <Slider
+              defaultValue={5}
+              min={1}
+              max={10}
+              step={1}
+              marks={difficultyMarks}
+              aria-label="Default"
+              valueLabelDisplay="auto"
+              value={difficulty}
+              onChange={handleDifficultyChange}
+              color="secondary"
+            />
+          </Box>
+          <Button variant="contained" color="secondary" sx={{ml: 15, mr: 15}}>Generate maze</Button>
         </div>
       </div>
-      {/* <motion.div
-          className="box"
-          animate={{ x, y }}
-          transition={{ type: "tween" }}
-        /> */}
+      {/* <div className="vertical-line"></div>
+      <div className="horizontal-line"></div> */}
     </div>
   );
 }
