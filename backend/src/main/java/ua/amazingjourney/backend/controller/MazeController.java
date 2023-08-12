a2ef5dd0e7709eeca16116e4e2dc7c40f0422da6
d3nnyyy <d3nnn41k@gmail.com>
2023-08-12 23:49:24 +0300

    added Javadoc for MazeController

--- backend/src/main/java/ua/amazingjourney/backend/controller/MazeController.java
+++ backend/src/main/java/ua/amazingjourney/backend/controller/MazeController.java
@@ -1,7 +1,11 @@
 package ua.amazingjourney.backend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import ua.amazingjourney.backend.model.MazeInitializer;
 import ua.amazingjourney.backend.model.MazeResponse;
 import ua.amazingjourney.backend.model.TraveledPathRequest;
@@ -9,14 +13,31 @@ import ua.amazingjourney.backend.service.MazeService;
 
 import java.util.LinkedList;
 
+/**
+ * Controller for maze-related requests.
+ * Contains methods for generating a maze,
+ * getting the shortest path in the maze
+ * and calculating the percentage of the traveled path's length
+ * compared to the shortest path's length.
+ */
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/maze")
 @RequiredArgsConstructor
 public class MazeController {
 
+    /**
+     * Injected MazeService instance.
+     */
     private final MazeService mazeService;
 
+    /**
+     * Generates a maze and returns the generated maze along with the shortest path.
+     *
+     * @param mazeInitializer The maze initialization parameters.
+     * @return MazeResponse containing the generated maze and its shortest path.
+     */
     @PostMapping("/generate")
     public MazeResponse generateMaze(@RequestBody MazeInitializer mazeInitializer) {
         boolean[][] generatedMaze = mazeService.generateMaze(mazeInitializer);
@@ -25,6 +46,12 @@ public class MazeController {
         return new MazeResponse(generatedMaze, shortestPath);
     }
 
+    /**
+     * Calculates and returns the percentage of the traveled path's length compared to the shortest path's length.
+     *
+     * @param traveledPathRequest The request containing the traveled path.
+     * @return The percentage of traveled path's length compared to the shortest path's length.
+     */
     @PostMapping("/stats")
     public Double getStats(@RequestBody TraveledPathRequest traveledPathRequest) {
         return mazeService.getStats(traveledPathRequest);
