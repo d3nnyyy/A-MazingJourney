6a49f9a3225ad2d49c26bf02976a2368ac841768
d3nnyyy <d3nnn41k@gmail.com>
2023-08-12 11:47:22 +0300

    removed all the classes which were responsible for getting the move request from frontend and modify character position, as we will not use it that way

--- backend/src/main/java/ua/amazingjourney/backend/model/CharacterMoveRequest.java
+++
@@ -1,9 +0,0 @@
-package ua.amazingjourney.backend.model;
-
-import lombok.Data;
-
-@Data
-public class CharacterMoveRequest {
-    private MoveRequest moveRequest;
-    private Character character;
-}
