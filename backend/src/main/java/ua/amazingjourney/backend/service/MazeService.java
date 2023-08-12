578542512193959b069184b5bda212e28d12b684
d3nnyyy <d3nnn41k@gmail.com>
2023-08-12 23:49:30 +0300

    added Javadoc for MazeService

--- backend/src/main/java/ua/amazingjourney/backend/service/MazeService.java
+++ backend/src/main/java/ua/amazingjourney/backend/service/MazeService.java
@@ -11,25 +11,54 @@ import ua.amazingjourney.backend.tools.MazeSolver;
 
 import java.util.LinkedList;
 
+/**
+ * Service for maze-related operations.
+ * Contains methods for generating a maze,
+ * getting the shortest path in the maze
+ * and calculating the percentage of the traveled path's length
+ * compared to the shortest path's length.
+ */
+
 @Service
 @Getter
 public class MazeService {
 
-    private Maze maze;
+    /**
+     * The maze instance.
+     */
+    private final Maze maze;
 
+    /**
+     * Constructor for MazeService class.
+     * Initializes the maze instance.
+     */
     public MazeService() {
         this.maze = new Maze();
     }
 
+    /**
+     * Generates a maze based on the provided maze initialization parameters.
+     *
+     * @param mazeInitializer The maze initialization parameters.
+     * @return The generated maze.
+     */
     public boolean[][] generateMaze(MazeInitializer mazeInitializer) {
         maze.setGrid(MazeGenerator.generateMaze(mazeInitializer));
         maze.setSize(mazeInitializer.getSize());
         return MazeGenerator.generateMaze(mazeInitializer);
     }
 
+    /**
+     * Gets the shortest path in the maze using BFS algorithm.
+     *
+     * @return A list of coordinates representing the shortest path.
+     */
     public LinkedList<LinkedList<Integer>> getShortestPath() {
 
-        LinkedList<Cell> solvedLinkedListOfCells = MazeSolver.solveMazeBFS(maze, new Cell(0, 0), new Cell(maze.getSize() * 2 - 2, maze.getSize() * 2 - 2));
+        LinkedList<Cell> solvedLinkedListOfCells = MazeSolver.solveMazeBFS(
+                maze,
+                new Cell(0, 0),
+                new Cell(maze.getSize() * 2 - 2, maze.getSize() * 2 - 2));
         LinkedList<LinkedList<Integer>> solvedLinkedListOfLinkedLists = new LinkedList<>();
 
         for (Cell cell : solvedLinkedListOfCells) {
@@ -41,6 +70,12 @@ public class MazeService {
         return solvedLinkedListOfLinkedLists;
     }
 
+    /**
+     * Calculates and returns the percentage of the traveled path's length compared to the shortest path's length.
+     *
+     * @param traveledPathRequest The request containing the traveled path.
+     * @return The percentage of traveled path's length compared to the shortest path's length.
+     */
     public Double getStats(TraveledPathRequest traveledPathRequest) {
 
         LinkedList<LinkedList<Integer>> traveledPath = traveledPathRequest.getVisitedCells();
