61ad2f7d8f0dd479a41d82e8873070c5b23024d1
hurrricane <maximgorak2005@gmail.com>
2023-08-12 19:37:48 +0300

    Added comments to maze initializer class

--- backend/src/main/java/ua/amazingjourney/backend/model/MazeInitializer.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/MazeInitializer.java
@@ -3,11 +3,24 @@ package ua.amazingjourney.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Initializer of maze. Used to pass as parameter in maze constructor.
+ */
 @Data
 @AllArgsConstructor
 public class MazeInitializer {
 
+    /**
+     * Size of a maze. Not a real size of an array in maze.
+     * The real size is this size * 2 - 1
+     */
     private int size;
+
+    /**
+     * Difficulty.
+     * The count of possible routes from start to finish depends on this variable.
+     * The less the difficulty, the less there are ways to pass the labyrinth.
+     */
     private int difficulty;
 
 }
