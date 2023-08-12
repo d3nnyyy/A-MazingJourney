42618115a76323e73cadc889321711356be657bd
hurrricane <maximgorak2005@gmail.com>
2023-08-12 19:15:43 +0300

    Added comments to maze class. Simplified it

--- backend/src/main/java/ua/amazingjourney/backend/model/Maze.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/Maze.java
@@ -5,34 +5,47 @@ import lombok.NoArgsConstructor;
 
 import static ua.amazingjourney.backend.tools.MazeGenerator.generateMaze;
 
+/**
+ * A maze. Has its grid and size
+ */
 @Data
 @NoArgsConstructor
 public class Maze {
 
-    /**
+    /*
      * Example of a maze:
-     * 01000100000 0
-     * 01010100000 1
-     * 01010001000 2
-     * 01011111000 3
-     * 00010000000 4
-     * 11110000000 5
-     * 01234567
+     * 010001 0
+     * 010101 1
+     * 010100 2
+     * 010111 3
+     * 000100 4
+     * 111100 5
+     * 012345
      * Where 1 is a wall
      * As we can see, there are walls on odd rows and columns
      * while blank spaces on even rows and columns
      **/
 
+    /**
+     * A grid. An array of true and false values, where true is a wall and false is a space for walking
+     */
     private boolean[][] grid;
+
+    /**
+     * A really walkable length of side of a maze
+     * the true size of a maze is this size * 2 - 1
+     * it will be the length of a side of a maze array
+     */
     private int size;
 
+    /**
+     * Default constructor. Creates a maze
+     * @param mazeInitializer an initializer, which has maze's data
+     */
     public Maze(MazeInitializer mazeInitializer) {
         this.grid = generateMaze(mazeInitializer);
         this.size = mazeInitializer.getSize();
     }
 
-    public boolean isWall(int x, int y) {
-        return grid[y][x];
-    }
 
 }
