8d8d73af06fe627be509bb29f42fa28c9754f814
d3nnyyy <d3nnn41k@gmail.com>
2023-08-12 23:49:17 +0300

    added Javadoc for MazeResponse

--- backend/src/main/java/ua/amazingjourney/backend/model/MazeResponse.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/MazeResponse.java
@@ -5,11 +5,24 @@ import lombok.Data;
 
 import java.util.LinkedList;
 
+/**
+ * Response for maze-related operations.
+ * Contains maze grid and shortest path in the maze.
+ * Used for convenient transfer of data to the front-end.
+ */
+
 @Data
 @AllArgsConstructor
 public class MazeResponse {
 
+    /**
+     * The maze grid.
+     */
     private boolean[][] maze;
+
+    /**
+     * The shortest path in the maze.
+     */
     private LinkedList<LinkedList<Integer>> shortestPath;
 
 }
