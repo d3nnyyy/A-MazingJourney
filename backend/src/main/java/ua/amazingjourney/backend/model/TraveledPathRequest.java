7421f4bb536feb00ad29cda7d19bdb88dec1f735
d3nnyyy <d3nnn41k@gmail.com>
2023-08-12 23:49:08 +0300

    added Javadoc for TraveledPathRequest

--- backend/src/main/java/ua/amazingjourney/backend/model/TraveledPathRequest.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/TraveledPathRequest.java
@@ -4,9 +4,20 @@ import lombok.Data;
 
 import java.util.LinkedList;
 
+/**
+ * Request for traveled path.
+ * Contains a list of visited cells.
+ * Used to get the data from front-end
+ * and being converted to a list of visited cells.
+ */
+
 @Data
 public class TraveledPathRequest {
 
+    /**
+     * A list of visited cells.
+     * Will be converted to a LinkedList of Cells.
+     */
     private LinkedList<LinkedList<Integer>> visitedCells;
 
 }
