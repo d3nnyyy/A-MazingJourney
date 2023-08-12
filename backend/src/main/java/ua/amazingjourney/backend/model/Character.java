502e025411ba9d6030692a8fb326d2ef9fdbfd8b
d3nnyyy <d3nnn41k@gmail.com>
2023-08-12 11:46:21 +0300

    removed all not used fields and methods in Character, there's only visitedCells field left

--- backend/src/main/java/ua/amazingjourney/backend/model/Character.java
+++ backend/src/main/java/ua/amazingjourney/backend/model/Character.java
@@ -1,5 +1,6 @@
 package ua.amazingjourney.backend.model;
 
+import lombok.NoArgsConstructor;
 import ua.amazingjourney.backend.tools.*;
 
 
@@ -12,60 +13,10 @@ import java.util.LinkedList;
  * The character. Can move
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Character {
-    //The row coordinate of a character
-    private int yCoordinate;
-
-    //The column coordinate of a character
-    private int xCoordinate;
 
     //The list of cells, that were visited by character
     private LinkedList<Cell> visitedCells;
-
-    public Character(int xCoordinate, int yCoordinate) {
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
-        visitedCells = new LinkedList<>();
-        visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
-    }
-
-    /**
-     * Moves character on 1 cell to the right
-     */
-    public void moveRight() {
-        xCoordinate += 2;
-        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
-            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
-        }
-    }
-
-    /**
-     * Moves character on 1 cell to the left
-     */
-    public void moveLeft() {
-        xCoordinate += 2;
-        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
-            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
-        }
-    }
-
-    /**
-     * Moves character on 1 cell up
-     */
-    public void moveUp() {
-        yCoordinate += 2;
-        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
-            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
-        }
-    }
-
-    /**
-     * Moves character on 1 cell down
-     */
-    public void moveDown() {
-        yCoordinate += 2;
-        if (!visitedCells.contains(new Cell(yCoordinate, xCoordinate))) {
-            visitedCells.addFirst(new Cell(yCoordinate, xCoordinate));
-        }
-    }
 }
