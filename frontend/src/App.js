08b192e3d622a273eca5c285708e001791526b45
d3nnyyy <d3nnn41k@gmail.com>
2023-08-16 20:41:42 +0300

    fixed Share button message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -116,7 +116,7 @@ function App() {
     const percentage = (userPathLength / shortestPathLength) * 100;
     const percentageDifference = Math.round((percentage - 100));
 
-    const subMessage = shortestPath === path ?
+    const subMessage = shortestPathLength === userPathLength ?
       "I've managed to solve the maze with the shortest path. Could you do it? " :
       `I've managed to solve the maze with the path which is ${percentageDifference}% longer than the shortest. I bet you could do it better! `
 
